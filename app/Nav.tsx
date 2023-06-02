4e8bf49e375b543de0d03888e9960e17531f899d
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2023-06-02 20:23:47 +0300

    logged user configs are added 💫

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Login from "./auth/Login";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
+import Logged from "./auth/Logged";
 
 export default async function Nav() {
   const session = await getServerSession(authOptions);
@@ -12,7 +13,7 @@ export default async function Nav() {
       </Link>
       <ul className="flex items-center gap-6">
         {!session?.user && <Login></Login>}
-        {session?.user && <h1>{session.user.name}</h1>}
+        {session?.user && <Logged image={session.user.image || ""}></Logged>}
       </ul>
     </nav>
   );
---
+++ app/auth/Logged.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+
+type User = {
+  image: string;
+};
+
+export default function Logged({ image }: User) {
+  return (
+    <li className="flex gap-8 items-center">
+      <button
+        onClick={() => signOut()}
+        className="bg-gray-700 text-sm text-white px-6 py-2 rounded-lg"
+      >
+        Sign Out
+      </button>
+      <Link href={"/dashboard"}>
+        <Image
+          width={64}
+          height={64}
+          src={image}
+          alt={""}
+          className="rounded-full"
+        ></Image>
+      </Link>
+    </li>
+  );
+}
